9e3bb075566269709737a923c7009d91e5e41805
Serhii Haiduchyk <jaegarn141@gmail.com>
2024-04-24 18:04:37 +0300

    refactor: refactoring code.

--- scraping_books/spiders/books.py
+++ scraping_books/spiders/books.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 import scrapy
 from scrapy.http import Response
 
@@ -7,7 +9,11 @@ class BooksSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
-    def parse(self, response: Response, **kwargs):
+    def parse(
+            self,
+            response: Response, **kwargs
+    ) -> Generator[scrapy.Request, None, None]:
+
         for product_url in response.css("h3 a::attr(href)").getall():
             product_url = response.urljoin(product_url)
             yield scrapy.Request(product_url, callback=self.parse_product)
@@ -16,13 +22,19 @@ class BooksSpider(scrapy.Spider):
         if next_page:
             yield response.follow(next_page, callback=self.parse)
 
-    def parse_product(self, response):
+    def extract_title(self, response: Response) -> str:
+        return response.css("h1::text").get()
+
+    def extract_price(self, response: Response) -> str:
+        return response.css(".price_color::text").get()
+
+    def parse_product(self, response: Response) -> None:
         yield {
-            "title": response.css("h1::text").get(),
-            "price": response.css(".price_color::text").get(),
-            "amount_in_stock": int(
-                response.css("p.instock.availability").re(r"\d+")[0]
-            ),
+            "title": self.extract_title(response),
+            "price": self.extract_price(response),
+            "amount_in_stock": int(response.css(
+                ".table.table-striped tr td::text"
+            ).getall()[-2].split()[-2].replace("(", "")),
             "rating": response.css(
                 "p.star-rating::attr(class)"
             ).get().split()[-1],
